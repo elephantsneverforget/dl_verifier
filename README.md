df48aa6f4f354a185376c96fac0cc39072805611
Jonathan Cairo <88749649+elephantsneverforget@users.noreply.github.com>
2021-11-27 18:08:02 -0700

    Add webpack instead of Rollup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 ## A GTM tag to verify the Elevar data layer for headless sites
-- Build `$npm run build` copy/paste into GTM tag.
+- Build `$npx webpack` copy/paste into GTM tag.
--- src/index.js
+++ src/index.js
@@ -1,24 +1,30 @@
-{/* <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css"> */ }
-{/* <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script> */ }
+{
+  /* <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css"> */
+}
+{
+  /* <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/toastify-js"></script> */
+}
 
-import { Logger } from './logger.js'
-import { DLViewItem } from './dataLayerObjectFormats/dlViewItem.js';
+import { Logger } from "./logger.js";
+import { DLViewItem } from "./dataLayerObjectFormats/dlViewItem.js";
 
 function evaluateDLEvent(dlEventId) {
-    const dlEventObect = dataLayer.find((dlEvent) => dlEvent['gtm.uniqueEventId'] === dlEventId);
-    const dlEventName = dlEventObect.event;
-    if (dlEventName in dlEventMap) {
-        const dlEvent = eventMap[dlEventName](dlEventObect);
-        dlEvent.verify()
-    }
+  const dlEventObect = dataLayer.find(
+    (dlEvent) => dlEvent["gtm.uniqueEventId"] === dlEventId
+  );
+  const dlEventName = dlEventObect.event;
+  if (dlEventName in dlEventMap) {
+    const dlEvent = eventMap[dlEventName](dlEventObect);
+    dlEvent.verify();
+  }
 }
 
 const dlEventMap = {
-    'dl_view_item': DLViewItem,
-    // 'dl_add_to_cart': DLAddToCart,
-}
+  dl_view_item: DLViewItem,
+  // 'dl_add_to_cart': DLAddToCart,
+};
 
-evaluateDLEvent('id')
-const dlViewItem = new DLViewItem({ event: 'dl_view_item', id: '3qwr' });
+evaluateDLEvent("id");
+const dlViewItem = new DLViewItem({ event: "dl_view_item", id: "3qwr" });
 console.log(dlViewItem.verify());
 // evaluateDLEvent({{dlv - DL Verifier - GTM unique event ID}});
